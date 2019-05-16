91fe4880a380abbc42ea2a2533091bb7adc4b839
Wpdas <wendersonpdas@gmail.com>
2019-05-16 03:29:47 -0300

    Adjustments

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 const cors = require('cors');
-app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204.
 
 app.use(express.static('public'));
 
